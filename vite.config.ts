bb463699fb6184ddd10e212cb7b56081c435f4e4
ascatena <adrisca27@alu.ing.unlp.edu.ar>
2025-04-14 23:17:19 +0000

    cambios en vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 // https://vitejs.dev/config/
 export default defineConfig({
 	base: './', // Fuerza siempre rutas relativas
-	
 	plugins: [svelte()],
+	build: {
+		outDir: 'dist',
+		emptyOutDir: true,
+		assetsInlineLimit: 4096 // (4KB) Archivos menores se inlinenan
+	  },
+	  publicDir: 'public' // Asegúrate que coincide con tu estructura
 });
